c3be5837c1b1541ccc95cb7b816526ede5d9827c
Thanh Ngan <thanhngan03052005@gmail.com>
2026-03-04 22:08:20 +0700

    Update HelloController

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -8,6 +8,17 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello from Spring Boot on cloud!";
+        return """
+               <h1>Giới thiệu nhóm</h1>
+               <p><b>Nhóm:</b> Nhóm 3</p>
+                <p><b>Thành viên:</b></p>
+           <ul>
+               <li>Nguyễn Văn A</li>
+               <li>Trần Thị B</li>
+               <li>Lê Văn C</li>
+           </ul>
+               <p><b>Đề tài demo:</b> Triển khai ứng dụng Spring Boot lên Cloud</p>
+               <p><b>Môn học:</b> Điện toán đám mây</p>
+               """;
     }
 }
