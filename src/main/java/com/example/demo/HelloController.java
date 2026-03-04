49a542f93ab2e0d1ea5b5eba878eefa3c5d23eb2
Thanh Ngan <thanhngan03052005@gmail.com>
2026-03-04 22:22:59 +0700

    Update hello content

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -10,12 +10,14 @@ public class HelloController {
     public String hello() {
         return """
                <h1>Giới thiệu nhóm</h1>
-               <p><b>Nhóm:</b> Nhóm 3</p>
-                <p><b>Thành viên:</b></p>
+               <p><b>Nhóm:</b> Nhóm Đại đại</p>
+               <p><b>Thành viên:</b></p>
            <ul>
-               <li>Nguyễn Văn A</li>
-               <li>Trần Thị B</li>
-               <li>Lê Văn C</li>
+               <li>Huỳnh Thanh Ngân</li>
+               <li>Lưu Viễn Dương</li>
+               <li>Hồ Như Trâm</li>
+               <li>Phetsaninda</li>
+               <li>Somphone</li>
            </ul>
                <p><b>Đề tài demo:</b> Triển khai ứng dụng Spring Boot lên Cloud</p>
                <p><b>Môn học:</b> Điện toán đám mây</p>
